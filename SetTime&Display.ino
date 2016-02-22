04c5d745084b53cfe77d15292492cbf095a91468
alexkychen <alexkychen@gmail.com>
2016-02-21 22:39:05 -0500

    Update and rename SetTime&Display to SetTime&Display.ino

--- SetTime&Display.ino
+++ SetTime&Display.ino
@@ -13,15 +13,15 @@ LiquidCrystal_I2C lcd(0x3F,16,2); //set LCD I2C address and tell it's a 16 by 2 
 //To get I2C address, follow http://playground.arduino.cc/Main/I2cScanner
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(4,0);
-  lcd.print("Welcome!");
-  lcd.setCursor(2,1);
-  lcd.print("Arduino Clock");
-  delay(2000);
-  lcd.clear();
-  delay(250);
+  lcd.init();               //initiate LCD display
+  lcd.backlight();          //turn on LCD backlight
+  lcd.setCursor(4,0);       //set cursor to 4th character and top row of the display
+  lcd.print("Welcome!");    //print "Welcome!" to the display
+  lcd.setCursor(2,1);        //set another cursor to 2th character and bottom row of the display
+  lcd.print("Arduino Clock"); //print "Arduino Clock" to the display
+  delay(2000);                //hold for 2 seconds
+  lcd.clear();                //clear the display
+  delay(250);              
   
   //Uncomment 1. & 2. when use RTC at the first time; comment 1. & 2. after time is set up.
   //setTime(20,27,20,21,2,2016); //1. manually set the current time to system in (hour,min,sec,day,month,year)
@@ -30,7 +30,7 @@ void setup() {
 }
 
 void loop() {
-  
+  //start printing time and date on the display, follow http://playground.arduino.cc/Code/Time to call time/date
   lcd.setCursor(0,0);
   lcd.print("TIME: ");
   lcd.print(hour());
